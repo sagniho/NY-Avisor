bee8c1315807912f74de2a06278efdf0b780d1d3
sagniho <79772266+sagniho@users.noreply.github.com>
2024-05-09 15:22:44 -0400

    Update new-york-guru.py

--- new-york-guru.py
+++ new-york-guru.py
@@ -1,43 +1,59 @@
-
 import streamlit as st
 from openai import OpenAI
 import time
-import streamlit as st
-import openai
-from uuid import uuid4
+
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-ASSISTANT_ID = st.secrets["NY_ADVISOR"]
 
-# Create columns for the logo and the title
-col1, col2 = st.columns([1, 4])
+# Define the advisors
+ADVISORS = {
+    "NY Solar Advisor": {
+        "id": st.secrets["NY_ADVISOR"],
+        "image": "ny.png",
+        "info_text": """
+        This AI assistant leverages GPT-4 technology with a knowledge cutoff in April 2024 to provide expert guidance on solar energy policies and incentives in New York. It integrates detailed document analysis for tailored, strategic advice.
 
-# In the first column, display the logo
-with col1:
-    st.image('ny.png', width=150)  # Adjust the width as needed
+        Key Documents in the Knowledge Base:
+        - 2024 Solar and Wind Tax Model: Guidance on tax models for solar and wind projects. Published by the New York State Tax Department.
+        - 2024_Solar_and_Wind_Appraisal_Model_User_Guide: Details on how to appraise solar and wind projects. Published by the New York State Tax Department.
+        - 2024-Gold-Book-Public: Comprehensive industry data and standards. Published by NYISO.
+        - New York Community Solar Policy Guide for Asset Owners & Developers: Guidelines for community solar projects. Created by Perch Energy's internal policy team.
+        - NY SUN Long Island + Upstate-Program-Manual: Program details for NY SUN's initiatives. Published by NYSERDA.
+        - NYSEG Queue Order by Substation: Latest queue order by substation for NYSEG. Published by NYSEG itself.
+        - Residential-SC-Program-Manual: Information on the residential solar credit program.
+        - Net Metering rules
+        - Value Stack Overview, Reference Guide: Published by NYSERDA.
+        """
+    },
+    "MD Solar Advisor": {
+        "id": st.secrets["MD_ADVISOR"],
+        "image": "maryland.png",
+        "info_text": """
+        This AI assistant provides guidance on solar energy policies and incentives in Maryland.
+        """
+    }
+}
 
-# In the second column, display the title and subtitle
-with col2:
-    st.markdown("<h2 style='margin-top: 0;padding-left: 10px;'> NY Solar Advisor</h2>", unsafe_allow_html=True)
-    st.markdown("<em><p style='margin-top: 0; padding-left: 10px;'>Your interactive AI-powered guide to Solar policies and incentives in the state of NY</p></em>", unsafe_allow_html=True)
+def display_advisors():
+    cols = st.columns(len(ADVISORS))
+    for i, (advisor, data) in enumerate(ADVISORS.items()):
+        with cols[i]:
+            st.image(data["image"], width=150)
+            st.write(advisor)
+            if st.button(f"Select {advisor}", key=f"select_{i}"):
+                st.session_state.selected_advisor = advisor
 
-# Information box with AI assistant capabilities and knowledge base
-info_text = """
-This AI assistant leverages GPT-4 technology with a knowledge cutoff in April 2024 to provide expert guidance on solar energy policies and incentives in New York. It integrates detailed document analysis for tailored, strategic advice.
+def display_selected_advisor():
+    advisor = st.session_state.selected_advisor
+    data = ADVISORS[advisor]
 
-Key Documents in the Knowledge Base:
-- 2024 Solar and Wind Tax Model: Guidance on tax models for solar and wind projects. Published by the New York State Tax Department.
-- 2024_Solar_and_Wind_Appraisal_Model_User_Guide: Details on how to appraise solar and wind projects. Published by the New York State Tax Department.
-- 2024-Gold-Book-Public: Comprehensive industry data and standards. Published by NYISO.
-- New York Community Solar Policy Guide for Asset Owners & Developers: Guidelines for community solar projects. Created by Perch Energy’s internal policy team.
-- NY SUN Long Island + Upstate-Program-Manual: Program details for NY SUN's initiatives. Published by NYSERDA.
-- NYSEG Queue Order by Substation: Latest queue order by substation for NYSEG. Published by NYSEG itself.
-- Residential-SC-Program-Manual: Information on the residential solar credit program.
-- Net Metering rules
-- Value Stack Overview, Reference Guide: Published by NYSERDA.
-"""
+    col1, col2 = st.columns([1, 4])
+    with col1:
+        st.image(data["image"], width=150)
+    with col2:
+        st.markdown(f"<h2 style='margin-top: 0;padding-left: 10px;'>{advisor}</h2>", unsafe_allow_html=True)
+        st.markdown("<em><p style='margin-top: 0; padding-left: 10px;'>Your interactive AI-powered guide to Solar policies and incentives</p></em>", unsafe_allow_html=True)
 
-
-st.info(info_text, icon="ℹ️")
+    st.info(data["info_text"], icon="ℹ️")
 
 def send_message_get_response(assistant_id, user_message):
     # Create a new thread
@@ -63,45 +79,41 @@ def send_message_get_response(assistant_id, user_message):
             text = latest_message.content[0].text.value
             return text
 
+def main():
+    st.sidebar.title("Select Advisor")
+    selected_advisor = st.sidebar.selectbox("Choose an advisor", list(ADVISORS.keys()))
+    st.session_state.selected_advisor = selected_advisor
 
+    if "selected_advisor" not in st.session_state:
+        display_advisors()
+    else:
+        display_selected_advisor()
 
-def main(): 
-    # Initialize messages in session state if not present
-    if 'thread' not in st.session_state:
-        st.session_state['thread'] = client.beta.threads.create().id
-    # Initialize messages in session state if not present
-    if 'messages' not in st.session_state:
-        st.session_state['messages'] = []
+        if 'thread' not in st.session_state:
+            st.session_state['thread'] = client.beta.threads.create().id
+        if 'messages' not in st.session_state:
+            st.session_state['messages'] = []
 
-    # Display previous chat messages
-    for msg in st.session_state.messages:
-        if msg['role'] == 'user':
+        for msg in st.session_state.messages:
+            if msg['role'] == 'user':
+                with st.chat_message("user", avatar="🧑‍💻"):
+                    st.write(msg["content"])
+            else:
+                with st.chat_message("assistant", avatar="☀️"):
+                    st.write(msg["content"])
+
+        user_input = st.chat_input(placeholder="Please ask me your question…")
+
+        if user_input:
+            st.session_state['messages'].append({'role': 'user', 'content': user_input})
             with st.chat_message("user", avatar="🧑‍💻"):
-                st.write(msg["content"])
-        else:
-            with st.chat_message("assistant", avatar="☀️"):
-                st.write(msg["content"])
+                    st.write(user_input)
 
-    # Chat input for new message
-    user_input = st.chat_input(placeholder="Please ask me your question…")
-
-    # When a message is sent through the chat input
-    if user_input:
-        # Append the user message to the session state
-        st.session_state['messages'].append({'role': 'user', 'content': user_input})
-        # Display the user message
-        with st.chat_message("user", avatar="🧑‍💻"):
-                st.write(user_input)
-
-        # Get the response from the assistant
-        with st.spinner('Working on this for you now...'):
-            response = send_message_get_response(ASSISTANT_ID, user_input)
-            # Append the response to the session state
-            st.session_state['messages'].append({'role': 'assistant', 'content': response})
-            # Display the assistant's response
-            with st.chat_message("assistant", avatar="☀️"):
-                st.write(response)
+            with st.spinner('Working on this for you now...'):
+                response = send_message_get_response(ADVISORS[selected_advisor]["id"], user_input)
+                st.session_state['messages'].append({'role': 'assistant', 'content': response})
+                with st.chat_message("assistant", avatar="☀️"):
+                    st.write(response)
 
 if __name__ == "__main__":
     main()
-
